c3fae69e4ae9f2be70e8a2b27ba94446d8677f40
ram2424 <ramsekhar2424@gmail.com>
2025-09-08 14:53:58 +0530

    uploading working codde to blink an led

--- rusty-blink/memory.x
+++ rusty-blink/memory.x
@@ -1,9 +1,14 @@
 MEMORY
 {
-
-    /* Flash memory begins at 0x08000000 and size is 128kB */
-    FLASH : ORIGIN = 0x08000000, LENGTH = 128K
-
-    /* RAM begins at 0x20000000 and size is 32kB */
-    RAM   : ORIGIN = 0x20000000, LENGTH = 32K
+  /* NOTE K = KiBi = 1024 bytes */
+  FLASH (rx) : ORIGIN = 0x08000000, LENGTH = 128K
+  RAM (rwx) : ORIGIN = 0x20000000, LENGTH = 32K
 }
+
+
+
+
+/* This is where the call stack will be allocated. */
+/* The stack is of the full descending type. */
+/* NOTE Do NOT modify `_stack_start` unless you know what you are doing */
+_stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- rusty-blink/src/main.rs
+++ rusty-blink/src/main.rs
@@ -1,44 +1,49 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+
+// Halt on panic
 use panic_halt as _;
 
-use hal::prelude::*;
-use hal::pwr::PwrExt;
-use hal::rcc::Config;
-use hal::stm32;
-use hal::time::ExtU32;
-use hal::timer::Timer;
-use stm32g4xx_hal as hal;
 
 use cortex_m_rt::entry;
+use cortex_m::delay::Delay;
+use stm32g4xx_hal::{
+    pac,
+    prelude::*,
+    rcc::Config,
+    stm32,
+};
 
+#[allow(clippy::empty_loop)]
 #[entry]
 fn main() -> ! {
-    // 1. Get access to the core and device peripherals
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32::Peripherals::take().unwrap();
-    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
-    // 2. Configure the clock system
-    let pwr = dp.PWR.constrain().freeze();
-    let mut rcc = dp.RCC.freeze(Config::hsi(), pwr);
+    dp.RCC.ahb2enr().modify(|_, w| w.gpioaen().set_bit());
 
-    // Configure clocks - use the correct API
-    let mut delay_syst = cp.SYST.delay(&rcc.clocks);
-
-    // 3. Configure the GPIO pin for the LED
+    
+    let mut rcc = dp.RCC.constrain();
     let mut gpioa = dp.GPIOA.split(&mut rcc);
+
     let mut led = gpioa.pa5.into_push_pull_output();
 
-    // 4. Configure Timer2 for delay
-    let timer2 = Timer::new(dp.TIM2, &rcc.clocks);
-	let mut delay_tim2 = timer2.start_count_down(100.millis()).delay();
+    // Create a delay provider using SysTick timer
+    let mut delay = Delay::new(cp.SYST, 16_000_000);
 
-    // 5. Loop forever, blinking the LED
     loop {
-        led.toggle();
-        delay_syst.delay(1000.millis());
-        led.toggle();
-        delay_tim2.delay_ms(1000);
+                // Turn LED on (set pin high)
+        led.set_high();
+        
+        // Wait for 500 milliseconds
+        delay.delay_ms(500u32);
+        
+        // Turn LED off (set pin low)
+        led.set_low();
+        
+        // Wait for 500 milliseconds
+        delay.delay_ms(500u32);
     }
 }
